32d4bd961eeef43c3320b61aaaa01e156ca80b1d
Simon <simonmansson@live.se>
2025-04-01 02:50:18 +0200

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import QualityPolicy from "@/components/qualitypolicy";
 export default function Home() {
   return (
     <div id="wrapper" className="bg-slate-100">
-      {/* <Header /> */}
+      
       <Hero />
       <Banner1 />
       <QualityPolicy />
